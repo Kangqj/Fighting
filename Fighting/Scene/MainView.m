35e99ec392fb138096e4f91c21a49ea3e8b570b8
Kangqj <tiankong.100@163.com>
2014-10-16 16:45:14 +0800

    new game
    一个简单的对战游戏

---
+++ Fighting/Scene/MainView.m
@@ -0,0 +1,35 @@
+//
+//  MainView.m
+//  Fighting
+//
+//  Created by kangqijun on 14-10-16.
+//  Copyright (c) 2014年 kangqijun. All rights reserved.
+//
+
+#import "MainView.h"
+
+@implementation MainView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        
+        self.backgroundColor = [UIColor grayColor];
+        
+        
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
